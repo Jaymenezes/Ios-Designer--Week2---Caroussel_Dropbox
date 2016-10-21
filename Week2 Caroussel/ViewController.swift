192f4951f1c1fb64a171669418f2bf134de75d94
JeanMenezes <jottamenezes@gmail.com>
2016-10-21 09:54:38 -0700

    First Commit
    Finished two first milestones. Created scroll view and added intro
    assets

---
+++ Week2 Caroussel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week2 Caroussel
+//
+//  Created by user on 10/21/16.
+//  Copyright © 2016 Jean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
